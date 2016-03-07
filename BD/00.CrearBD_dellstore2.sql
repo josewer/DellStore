0fde936fd00bbb6ba24507c2adc838f3233e0c86
josewer <jose.luis.torregrosa1011@gmail.com>
2016-03-07 21:16:09 +0100

    Añado BD

---
+++ BD/00.CrearBD_dellstore2.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE dellstore2
+  WITH OWNER = postgres
+       TABLESPACE = pg_default
+       CONNECTION LIMIT = -1;
+
+ CREATE ROLE dellstore2  
+ WITH LOGIN PASSWORD 'dellstore2'
+  SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
+  
+  
+  ' Cargar los datos desde consola 
+ ' psql -U dellstore2 -d dellstore2 -f normal.sql -h localhost
